aa3e7e84a4a5b05475dff27a19167e3af9ed165d
higekick <higekick39@gmail.com>
2020-07-24 02:03:57 +0900

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      # 左ページのtweetポスト画面へ
+      @micropost  = current_user.microposts.build
+      # 右ページのtweet一覧へ
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
